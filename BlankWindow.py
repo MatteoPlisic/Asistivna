b255570bc2e90a289202209c0e08bc919a9c4b56
Matteo Plišić <77995521+MatteoPlisic@users.noreply.github.com>
2023-12-29 16:23:19 +0100

    First commit

--- BlankWindow.py
+++ BlankWindow.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 class BlankWindow:
     def __init__(self, root, title="Blank Window", width=400, height=300):
@@ -14,6 +15,42 @@ class BlankWindow:
 
         self.root.geometry(f"{width}x{height}+{x}+{y}")
 
-        # Create widgets or add additional setup as needed
-        label = tk.Label(root, text="This is a blank window.")
-        label.pack(pady=20)
+        # Create a 4x4 grid of tiles
+        self.create_tiles()
+
+    def create_tiles(self):
+        # Create a 4x4 grid of tiles
+        for i in range(4):
+            for j in range(4):
+                tile_text = f"Tile {i * 4 + j + 1}"  # Text for the tile
+                tile_number = i * 4 + j + 1  # Number for the tile
+
+                # Create a tile with the specified text and number
+                tile_button = tk.Button(
+                    self.root,
+                    text=f"{tile_text}\n{tile_number}",
+                    command=lambda text=tile_text, number=tile_number: self.tile_clicked(text, number)
+                )
+
+                # Bind mouse events
+                tile_button.bind("<Enter>", lambda event, button=tile_button: self.mouse_enter(event, button))
+                tile_button.bind("<Leave>", lambda event, button=tile_button: self.mouse_leave(event, button))
+
+                # Grid layout
+                tile_button.grid(row=i, column=j, padx=5, pady=5)
+
+    def tile_clicked(self, text, number):
+        print(f"Tile Clicked! Text: {text}, Number: {number}")
+
+    def mouse_enter(self, event, button):
+        # Set a callback to be executed after 2000 milliseconds (2 seconds)
+        button.after(2000, lambda: self.simulate_click(button))
+
+    def mouse_leave(self, event, button):
+        # Cancel the callback when the mouse leaves the button
+        button.after_cancel(self.simulate_click)
+
+    def simulate_click(self, button):
+        # Simulate a click when the button has been hovered for more than 2 seconds
+        button.event_generate("<Button-1>")
+        print("clicked tile ")
