29e54e084a9dfa7f8c2b3488192e28f1d12f50c0
Matteo Plišić <77995521+MatteoPlisic@users.noreply.github.com>
2024-01-09 22:36:11 +0100

    Adding things

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,26 +1,28 @@
-w, h = 1, 101
-arr = [[0 for x in range(w)] for y in range(h)]
-print(arr, "before")
+def cheatsheet():
+    w, h = 1, 101
+    arr = [[0 for x in range(w)] for y in range(h)]
+    print(arr, "before")
 
-print(arr, "after")
+    print(arr, "after")
 
-file_path = 'mappings.txt'
+    file_path = 'mappings.txt'
 
-with open(file_path, 'r') as file:
-    for line in file:
+    with open(file_path, 'r') as file:
+        for line in file:
 
-        numbers = list(map(int, line.split()))
+            numbers = list(map(int, line.split()))
 
-        question_number = numbers[0]
-        answers = numbers[1:]
+            question_number = numbers[0]
+            answers = numbers[1:]
 
-        print(f"Question {question_number}: Answers {answers}")
-        for number in answers:
-            arr[question_number].append(number)
+            print(f"Question {question_number}: Answers {answers}")
+            for number in answers:
+                arr[question_number].append(number)
 
-print(arr, "gotovo")
+    #print(arr, "gotovo")
 
-for i in range(100):
-    arr[i][0] = i
+    for i in range(100):
+        arr[i][0] = i
 
-print(arr, "gotovo")
+    print(arr, "gotovo")
+    return arr
